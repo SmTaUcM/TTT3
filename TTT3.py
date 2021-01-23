48dc41cab2f6c1588faf2ac61400023fcbc7125f
Stuart Macintosh <SmTaUcM@Yahoo.co.uk>
2021-01-23 09:59:22 +0000

    Version Bump to Alpha 11

--- TTT3.py
+++ TTT3.py
@@ -46,8 +46,8 @@ class TTT3(QMainWindow):
         try:
             # Version info.
             version = "3.00"
-            devVersion = "Alpha 10"
-            date = "22 January 2021"
+            devVersion = "Alpha 11"
+            date = "23 January 2021"
 
             # Initialise an instance of a QT Main Window and load our GUI file 'data\uis\ttt.ui'.
             QMainWindow.__init__(self)
