89d33a9bd958200c42b0f73d0eaab0e5f2228e68
Stuart Macintosh <SmTaUcM@Yahoo.co.uk>
2021-02-04 08:18:22 +0000

    Misc updates, trims and collars.

--- Useful Info/Dependency Installer.bat
+++ Useful Info/Dependency Installer.bat
@@ -7,6 +7,7 @@ python -m pip install pillow
 python -m pip install opencv-python
 python -m pip install pyinstaller
 python -m pip install --upgrade autopep8
+python -m pip install urllib3
 echo
 echo Installation complete...
 echo
